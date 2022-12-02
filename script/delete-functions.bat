ad3c73cba52efecdcb052113e78c7d9bae42baf2
hsmtkk <hsmtkk@gmail.com>
2022-12-03 07:38:28 +0900

    script to delete cloud functions

---
+++ script/delete-functions.bat
@@ -0,0 +1,3 @@
+gcloud functions delete ocr-extract --gen2 --quiet --region asia-northeast1
+gcloud functions delete ocr-save --gen2 --quiet --region asia-northeast1
+gcloud functions delete ocr-translate --gen2 --quiet --region asia-northeast1
