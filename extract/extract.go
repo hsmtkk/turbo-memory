4505f08d6b305357f35a49f1f0cce56109a4380f
hsmtkk <hsmtkk@gmail.com>
2022-12-03 09:26:16 +0900

    implemented extract function

--- extract/extract.go
+++ extract/extract.go
@@ -4,8 +4,13 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
+	"strings"
 	"time"
 
+	"cloud.google.com/go/pubsub"
+	"cloud.google.com/go/translate"
+	vision "cloud.google.com/go/vision/apiv1"
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 	"github.com/cloudevents/sdk-go/v2/event"
 )
@@ -34,28 +39,105 @@ func extract(ctx context.Context, evt event.Event) error {
 	}
 	log.Printf("%v\n", data)
 
+	if err := detectText(ctx, data.Bucket, data.Name); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func detectText(ctx context.Context, bucket, name string) error {
+	texts, err := callVision(ctx, bucket, name)
+	if err != nil {
+		return err
+	}
+	if err := callTranslate(ctx, texts); err != nil {
+		return err
+	}
+
+	projectID := os.Getenv("GCP_PROJECT")
+	toLang := os.Getenv("TO_LANG")
+	topic := os.Getenv("TRANSLATE_TOPIC")
+	langs := strings.Split(toLang, ",")
+	text := strings.Join(texts, " ")
+
+	for _, lang := range langs {
+		attrs := map[string]string{
+			"text":     text,
+			"filename": name,
+			"lang":     lang,
+		}
+		if err := publishData(ctx, projectID, topic, attrs); err != nil {
+			log.Printf("publishData failed; %v", err.Error())
+		}
+		log.Printf("published: %v\n", attrs)
+	}
+
+	return nil
+}
+
+func callVision(ctx context.Context, bucket, name string) ([]string, error) {
+	visionClt, err := vision.NewImageAnnotatorClient(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("vision.NewImageAnnotatorClient failed; %w", err)
+	}
+	url := fmt.Sprintf("gs://%s/%s", bucket, name)
+	img := vision.NewImageFromURI(url)
+	annotations, err := visionClt.DetectTexts(ctx, img, nil, 10)
+	if err != nil {
+		return nil, fmt.Errorf("vision.ImageAnnotatorClient.DetectTexts failed; %w", err)
+	}
+	texts := []string{}
+	for _, annotation := range annotations {
+		texts = append(texts, annotation.Description)
+	}
+	log.Printf("detected texts: %v\n", texts)
+	return texts, nil
+}
+
+func callTranslate(ctx context.Context, texts []string) error {
+	transClient, err := translate.NewClient(ctx)
+	if err != nil {
+		return fmt.Errorf("translate.NewClient failed; %w", err)
+	}
+	detections, err := transClient.DetectLanguage(ctx, texts)
+	if err != nil {
+		return fmt.Errorf("translate.Client.DetectLanguage failed; %w", err)
+	}
+	langSet := map[string]bool{}
+	for _, detects := range detections {
+		for _, detect := range detects {
+			langSet[detect.Language.String()] = true
+		}
+	}
+	langs := []string{}
+	for lang := range langSet {
+		langs = append(langs, lang)
+	}
+	log.Printf("detected languages: %v\n", langs)
+	return nil
+}
+
+func publishData(ctx context.Context, projectID, topicName string, attrs map[string]string) error {
+	clt, err := pubsub.NewClient(ctx, projectID)
+	if err != nil {
+		return fmt.Errorf("pubsub.NewClient failed; %w", err)
+	}
+	defer clt.Close()
+	topic := clt.Topic(topicName)
+	result := topic.Publish(ctx, &pubsub.Message{
+		Data:       []byte("extract"),
+		Attributes: attrs,
+	})
+	id, err := result.Get(ctx)
+	if err != nil {
+		return fmt.Errorf("pubsub.PublishResult.Get failed; %w", err)
+	}
+	log.Printf("published: %s\n", id)
 	return nil
 }
 
 /*
-exports.processImage = async event => {
-  const {bucket, name} = event;
-
-  if (!bucket) {
-    throw new Error(
-      'Bucket not provided. Make sure you have a "bucket" property in your request'
-    );
-  }
-  if (!name) {
-    throw new Error(
-      'Filename not provided. Make sure you have a "name" property in your request'
-    );
-  }
-
-  await detectText(bucket, name);
-  console.log(`File ${name} processed.`);
-};
-
 const detectText = async (bucketName, filename) => {
   console.log(`Looking for text in image ${filename}`);
   const [textDetections] = await vision.textDetection(
